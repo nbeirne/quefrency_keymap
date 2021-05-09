8c5892d05d7900252ba05fc1ba174046cb26b866
Nick <6374684+nbeirne@users.noreply.github.com>
2021-05-09 15:43:31 -0400

    tweaks

--- keymap.c
+++ keymap.c
@@ -18,28 +18,34 @@ enum encoders {
 
 
 // mac keys
+
 #define MC_LOPT KC_LALT
 #define MC_ROPT KC_RALT
 
 #define MC_LCMD KC_LGUI
 #define MC_RCMD KC_RGUI
 
+
+// emoji popup
+//#define EMOJI LCMD(LGUI(KC_SPC))
+#define EMOJI LCTL(LGUI(KC_SPC))
+// spotlight / alphred
+#define SPOT LCMD(KC_SPC)
+// mute with mutekey
 #define MUTE LGUI(S(KC_W))
-#define XC_B LGUI(KC_B)
-#define XC_R LGUI(KC_R)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_65_with_macro(
 // ┌────────┬────────┐  ┌────────┬────────┬────────┬────────┬────────┬────────┬────────┐        ┌────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┐
-    KC_F1,   KC_ESC,     KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,             KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_DEL,  KC_BSPC, KC_MUTE, \
+    KC_F1,   KC_ESC,     KC_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,             KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_DEL,  KC_BSPC, KC_MUTE, \
 // ├────────┼────────┤  ├────────┴────┬───┴────┬───┴────┬───┴────┬───┴────┬───┴────┬───┘    ┌───┴────┬───┴────┬───┴────┬───┴────┬───┴────┬───┴────┬───┴────┬───┴────────┼────────┤
-    KC_F2,   KC_F3,      KC_TAB,       KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,     KC_DEL, \
+    EMOJI,   SPOT,       KC_TAB,       KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,     KC_DEL, \
 // ├────────┼────────┤  ├─────────────┴─┬──────┴─┬──────┴─┬──────┴─┬──────┴─┬──────┴─┐      └─┬──────┴─┬──────┴─┬──────┴─┬──────┴─┬──────┴─┬──────┴─┬──────┴────────────┼────────┤
     KC_F4,   KC_F5,      KC_LCTRL,       KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,             KC_PGUP, \
 // ├────────┼────────┤  ├───────────────┴─────┬──┴─────┬──┴─────┬──┴─────┬──┴─────┬──┴─────┐  └─────┬──┴─────┬──┴─────┬──┴─────┬──┴─────┬──┴─────┬──┴──────────┬────────┼────────┤
-    MUTE,    KC_F8,      KC_LSFT,              KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,      KC_UP,   KC_PGDN, \
+    KC_MPRV, KC_MNXT,    KC_LSFT,              KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,      KC_UP,   KC_PGDN, \
 // ├────────┼────────┤  ├──────────┬──────────┴┬───────┴──┬─────┴─────┬──┴────────┴────────┤        ├────────┴────────┴────┬───┴────┬───┴────┬───┴────┬────────┼────────┼────────┤
-    XC_B,    XC_R,       MO(_FN1),  KC_LCTRL,   KC_LOPT,   MC_LCMD,    KC_BSPC,                      _______, KC_SPC,       MC_RCMD, KC_ROPT, _______, KC_LEFT, KC_DOWN, KC_RGHT
+    MUTE,    KC_MPLY,    MO(_FN1),  KC_LCTRL,   KC_LOPT,   MC_LCMD,    KC_BSPC,                      _______, KC_SPC,       MC_RCMD, KC_ROPT, _______, KC_LEFT, KC_DOWN, KC_RGHT
 // └────────┴────────┘  └──────────┴───────────┴──────────┴───────────┴────────────────────┘        └──────────────────────┴────────┴────────┘        └────────┴────────┴────────┘
   ),
 
